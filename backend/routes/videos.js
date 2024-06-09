4057476bbd758043ae771bcddf97e55a6facb84a
Subhendu Sekhar Pradhan <Sspradhan25@hotmail.com>
2024-06-10 05:22:00 +0530

    Update videos.js

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const Video = require('../models/Video');
+const { Video } = require('../models');
 const router = express.Router();
 
 // CRUD operations for videos
 router.post('/', async (req, res) => {
-    const video = new Video(req.body);
-    await video.save();
-    res.send(video);
+  const video = await Video.create(req.body);
+  res.send(video);
 });
 
 router.get('/', async (req, res) => {
-    const videos = await Video.find();
-    res.send(videos);
+  const videos = await Video.findAll();
+  res.send(videos);
 });
 
 router.get('/:id', async (req, res) => {
-    const video = await Video.findById(req.params.id);
-    res.send(video);
+  const video = await Video.findByPk(req.params.id);
+  res.send(video);
 });
 
 router.put('/:id', async (req, res) => {
-    const video = await Video.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.send(video);
+  await Video.update(req.body, { where: { id: req.params.id } });
+  const updatedVideo = await Video.findByPk(req.params.id);
+  res.send(updatedVideo);
 });
 
 router.delete('/:id', async (req, res) => {
-    await Video.findByIdAndDelete(req.params.id);
-    res.send({ message: 'Video deleted' });
+  await Video.destroy({ where: { id: req.params.id } });
+  res.send({ message: 'Video deleted' });
 });
 
 module.exports = router;
