2ac34a4190445a6926ee1e9ea291f2a00c2ad5cb
Subhendu Sekhar Pradhan <Sspradhan25@hotmail.com>
2024-06-10 05:21:23 +0530

    Update categories.js

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const Category = require('../models/Category');
+const { Category } = require('../models');
 const router = express.Router();
 
 // CRUD operations for categories
 router.post('/', async (req, res) => {
-    const category = new Category(req.body);
-    await category.save();
-    res.send(category);
+  const category = await Category.create(req.body);
+  res.send(category);
 });
 
 router.get('/', async (req, res) => {
-    const categories = await Category.find();
-    res.send(categories);
+  const categories = await Category.findAll();
+  res.send(categories);
 });
 
 router.get('/:id', async (req, res) => {
-    const category = await Category.findById(req.params.id);
-    res.send(category);
+  const category = await Category.findByPk(req.params.id);
+  res.send(category);
 });
 
 router.put('/:id', async (req, res) => {
-    const category = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.send(category);
+  await Category.update(req.body, { where: { id: req.params.id } });
+  const updatedCategory = await Category.findByPk(req.params.id);
+  res.send(updatedCategory);
 });
 
 router.delete('/:id', async (req, res) => {
-    await Category.findByIdAndDelete(req.params.id);
-    res.send({ message: 'Category deleted' });
+  await Category.destroy({ where: { id: req.params.id } });
+  res.send({ message: 'Category deleted' });
 });
 
 module.exports = router;
